8d4d47844d3b18180b924f06f46897e298cfbf12
Soultan <soultan_98@hotmail.com>
2022-07-05 14:42:46 +0200

    added TODOs

---
+++ lib/uis/home_page.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/widgets.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  State<StatefulWidget> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  // TODO : Statistique de consommation des 7 derniers jours :
+  // - Lipides
+  // - Glucide
+  // - Proteines
+  // TODO : Statistiques sur les vitamines consommés et les carences
+  // TODO : Statistiques sur les KCal consommés
+
+  @override
+  Widget build(BuildContext context) {
+    throw UnimplementedError();
+  }
+}
