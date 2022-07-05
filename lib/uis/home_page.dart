16017600d03fd7f1dfbc3f4e8b4ace89faf37e30
Soultan <soultan_98@hotmail.com>
2022-07-05 09:01:39 +0200

    implemented navigation bar with floating button

--- lib/uis/home_page.dart
+++ lib/uis/home_page.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 
 class HomePage extends StatefulWidget {
@@ -10,6 +11,50 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    throw UnimplementedError();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('My fitness pal'),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () => _navigateCamera,
+        child: const Icon(Icons.camera_alt_outlined),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+      bottomNavigationBar: BottomAppBar(
+        color: Colors.lightBlueAccent,
+        shape: const CircularNotchedRectangle(),
+        notchMargin: 5,
+        child: Row(
+          mainAxisSize: MainAxisSize.max,
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: <Widget>[
+            IconButton(
+              icon: const Icon(
+                Icons.home_outlined,
+                color: Colors.white,
+              ),
+              onPressed: () {},
+            ),
+            IconButton(
+              icon: const Icon(
+                Icons.question_mark,
+                color: Colors.white,
+              ),
+              onPressed: () {},
+            ),
+            IconButton(
+              icon: const Icon(
+                Icons.settings_outlined,
+                color: Colors.white,
+              ),
+              onPressed: () {},
+              focusColor: Colors.blue,
+            )
+          ],
+        ),
+      ),
+    );
   }
+
+  void _navigateCamera() {}
 }
