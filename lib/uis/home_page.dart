abcd4c4d5fcaa933f846a32da27a0a9a60c92978
Soultan <soultan_98@hotmail.com>
2022-07-05 08:50:14 +0200

    created home page

---
+++ lib/uis/home_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/widgets.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  State<StatefulWidget> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    throw UnimplementedError();
+  }
+}
