43fc7d5bbe7fe6854f45397a3c0f33a0f955d579
Soultan <soultan_98@hotmail.com>
2022-07-05 14:17:47 +0200

    created history_page

---
+++ lib/uis/history_page.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/widgets.dart';
+
+class HistoryPage extends StatefulWidget {
+  const HistoryPage({Key? key}) : super(key: key);
+
+  @override
+  State<StatefulWidget> createState() {
+    return _HistoryPageState();
+  }
+}
+
+class _HistoryPageState extends State<HistoryPage> {
+  @override
+  Widget build(BuildContext context) {
+    throw UnimplementedError();
+  }
+}
