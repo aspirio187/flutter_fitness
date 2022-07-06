b40297bb925330213b081e923703d2159fb7c90f
Soultan <soultan_98@hotmail.com>
2022-07-06 11:32:11 +0200

    created getProduct(barcode) function

--- lib/services/product_service.dart
+++ lib/services/product_service.dart
@@ -1,1 +1,15 @@
-class ProductService {}
+import 'package:openfoodfacts/openfoodfacts.dart';
+
+class ProductService {
+  ProductService();
+
+  getProduct(String barcode) async {
+    var configuration = ProductQueryConfiguration(barcode);
+
+    ProductResult result = await OpenFoodAPIClient.getProduct(configuration);
+
+    if (result.status != 1) {
+      return;
+    }
+  }
+}
