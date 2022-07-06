c15e73a9340822c19639307687069c5fd267471a
Soultan <soultan_98@hotmail.com>
2022-07-06 14:08:55 +0200

    sqlite implementation

--- lib/services/product_service.dart
+++ lib/services/product_service.dart
@@ -1,10 +1,18 @@
+import 'package:flutter/widgets.dart';
 import 'package:flutter_fitness/models/product_model.dart';
+import 'package:flutter_fitness/sql_helper.dart';
 import 'package:openfoodfacts/openfoodfacts.dart';
+
+import 'dart:async';
+
+import 'package:path/path.dart' as p;
+import 'package:path_provider/path_provider.dart';
+import 'package:sqflite/sqflite.dart' as sql;
 
 class ProductService {
   ProductService();
 
-  Future<ProductModel?> getProduct(String barcode) async {
+  Future<ProductModel?> getProductFromSDK(String barcode) async {
     var configuration = ProductQueryConfiguration(barcode);
 
     ProductResult result = await OpenFoodAPIClient.getProduct(configuration);
@@ -20,5 +28,39 @@ class ProductService {
     ProductModel product = ProductModel.fromSDK(result.product!);
 
     return product;
+  }
+
+  Future<int> createProduct(ProductModel product) async {
+    final db = await SqlHelper.db();
+
+    final data = {
+      'barcode': product.barcode,
+      'name': product.name,
+      'imgUrl': product.imgUrl,
+      'imgSmallUrl': product.imgSmallUrl,
+      'carboHydrates': product.carboHydrates,
+      'protein': product.protein,
+      'fat': product.fat,
+      'energyKj': product.energyKj,
+      'energyKcal': product.energyKcal,
+      'vitaminA': product.vitaminA,
+      'vitaminB1': product.vitaminB1,
+      'vitaminB2': product.vitaminB2,
+      'vitaminB6': product.vitaminB6,
+      'vitaminB9': product.vitaminB9,
+      'vitaminC': product.vitaminC,
+      'vitaminD': product.vitaminD,
+      'vitaminE': product.vitaminE,
+      'vitaminK': product.vitaminK,
+      'vitaminPP': product.vitaminPP
+    };
+
+    final id = await db.insert(
+      'products',
+      data,
+      conflictAlgorithm: sql.ConflictAlgorithm.replace,
+    );
+
+    return id;
   }
 }
---
+++ lib/sql_helper.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/foundation.dart';
+import 'package:flutter/widgets.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:sqflite/sqflite.dart' as sql;
+import 'package:path/path.dart' as p;
+
+class SqlHelper {
+  static Future<void> createTables(sql.Database database) async {
+    await database.execute('''
+      CREATE TABLE products(
+        id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+        barcode TEXT NOT NULL,
+        name TEXT NOT NULL,
+        imgUrl TEXT NOT NULL,
+        imgSmallUrl TEXT NOT NULL,
+        carboHydrates REAL NOT NULL,
+        protein REAL NOT NULL,
+        fat REAL NOT NULL,
+        energyKj REAL NOT NULL,
+        energyKcal REAL NOT NULL,
+        vitaminA REAL NOT NULL,
+        vitaminB1 REAL NOT NULL,
+        vitaminB2 REAL NOT NULL,
+        vitaminB6 REAL NOT NULL,
+        vitaminB9 REAL NOT NULL,
+        vitaminC REAL NOT NULL,
+        vitaminD REAL NOT NULL,
+        vitaminE REAL NOT NULL,
+        vitaminK REAL NOT NULL,
+        vitaminPP REAL NOT NULL,
+      );
+      ''');
+  }
+
+  static Future<sql.Database> db() async {
+    final docDir = await getApplicationDocumentsDirectory();
+    if (!await docDir.exists()) {
+      await docDir.create(recursive: true);
+    }
+
+    String dbPath = p.join(docDir.path, 'myfitnesspal.db');
+
+    return sql.openDatabase(
+      dbPath,
+      version: 1,
+      onCreate: (sql.Database database, int version) async {
+        await createTables(database);
+      },
+    );
+  }
+}
