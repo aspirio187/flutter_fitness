47a0d03059c4d8271977d88d9a85a1d818d0e7cf
Soultan <soultan_98@hotmail.com>
2022-07-07 12:36:30 +0200

    fixed product detail screen

--- lib/uis/product_detail_screen.dart
+++ lib/uis/product_detail_screen.dart
@@ -19,172 +19,191 @@ class _ProductDetailScreenState extends State<ProductDetailScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return SingleChildScrollView(
-      child: FutureBuilder<ProductModel?>(
-        future: _productService.getProductFromSDK(widget.barcode),
-        builder: (BuildContext context, AsyncSnapshot<ProductModel?> snapshot) {
-          if (snapshot.hasData) {
-            if (snapshot.data == null) {
-              return Center(
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.barcode),
+      ),
+      body: SingleChildScrollView(
+        padding: const EdgeInsets.all(10),
+        child: FutureBuilder<ProductModel?>(
+          future: _productService.getProductFromSDK(widget.barcode),
+          builder:
+              (BuildContext context, AsyncSnapshot<ProductModel?> snapshot) {
+            if (snapshot.hasData) {
+              if (snapshot.data == null) {
+                return Center(
+                  child: Text(
+                    'Aucun produit avec le code bar ${widget.barcode} n\'a été '
+                    'trouvé!',
+                  ),
+                );
+              }
+
+              ProductModel product = snapshot.data!;
+
+              return Column(
+                children: [
+                  Image.network(product.imgUrl),
+                  Text(
+                    product.name,
+                    style: const TextStyle(
+                      fontSize: 24,
+                    ),
+                  ),
+                  const Text(
+                    'Les informations suivantes représente le nombre de gramme '
+                    '(g) par portion de 100 grammes',
+                  ),
+                  const Divider(),
+                  Table(
+                    columnWidths: <int, TableColumnWidth>{
+                      0: FixedColumnWidth(
+                          MediaQuery.of(context).size.width / 100 * 50),
+                      1: const IntrinsicColumnWidth(),
+                    },
+                    defaultVerticalAlignment: TableCellVerticalAlignment.middle,
+                    children: <TableRow>[
+                      const TableRow(
+                        children: [
+                          TableCell(
+                            child: Text(
+                              'Nutriment',
+                              style: TextStyle(
+                                fontSize: 18,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                          ),
+                          TableCell(
+                            child: Text(
+                              'Quantité (en g)',
+                              style: TextStyle(
+                                fontSize: 18,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                      TableRow(
+                        children: [
+                          const TableCell(
+                            child: Text(
+                              'Glucides',
+                            ),
+                          ),
+                          TableCell(
+                            verticalAlignment:
+                                TableCellVerticalAlignment.middle,
+                            child: Text(
+                              product.carboHydrates.toString(),
+                            ),
+                          ),
+                        ],
+                      ),
+                      TableRow(
+                        children: [
+                          const TableCell(
+                            child: Text('Protides'),
+                          ),
+                          TableCell(
+                            child: Text(
+                              product.protein.toString(),
+                            ),
+                          ),
+                        ],
+                      ),
+                      TableRow(
+                        children: [
+                          const TableCell(
+                            child: Text(
+                              'Lipides',
+                            ),
+                          ),
+                          TableCell(
+                            child: Text(
+                              product.fat.toString(),
+                            ),
+                          ),
+                        ],
+                      ),
+                      TableRow(
+                        children: [
+                          const Text('Vitamine A'),
+                          Text(product.vitaminA.toString()),
+                        ],
+                      ),
+                      TableRow(
+                        children: [
+                          const Text('Vitamine B1'),
+                          Text(product.vitaminB1.toString()),
+                        ],
+                      ),
+                      TableRow(
+                        children: [
+                          const Text('Vitamine B2'),
+                          Text(product.vitaminB2.toString()),
+                        ],
+                      ),
+                      TableRow(
+                        children: [
+                          const Text('Vitamine B6'),
+                          Text(product.vitaminB6.toString()),
+                        ],
+                      ),
+                      TableRow(
+                        children: [
+                          const Text('Vitamine B9'),
+                          Text(product.vitaminB9.toString()),
+                        ],
+                      ),
+                      TableRow(
+                        children: [
+                          const Text('Vitamine C'),
+                          Text(product.vitaminC.toString()),
+                        ],
+                      ),
+                      TableRow(
+                        children: [
+                          const Text('Vitamine D'),
+                          Text(product.vitaminD.toString()),
+                        ],
+                      ),
+                      TableRow(
+                        children: [
+                          const Text('Vitamine E'),
+                          Text(product.vitaminE.toString()),
+                        ],
+                      ),
+                      TableRow(
+                        children: [
+                          const Text('Vitamine K'),
+                          Text(product.vitaminK.toString()),
+                        ],
+                      ),
+                      TableRow(
+                        children: [
+                          const Text('Vitamine PP'),
+                          Text(product.vitaminPP.toString()),
+                        ],
+                      ),
+                    ],
+                  )
+                ],
+              );
+            } else if (snapshot.hasError) {
+              return const Center(
                 child: Text(
-                  'Aucun produit avec le code bar ${widget.barcode} n\'a été '
-                  'trouvé!',
+                  'Une erreur est survenu lors du chargement des données',
                 ),
               );
+            } else {
+              return const Center(
+                child: CircularProgressIndicator(),
+              );
             }
-
-            ProductModel product = snapshot.data!;
-
-            return Column(
-              children: [
-                Image.network(product.imgUrl),
-                Text(
-                  product.name,
-                  style: const TextStyle(
-                    fontSize: 24,
-                  ),
-                ),
-                const Text(
-                  'Les informations suivantes représente le nombre de gramme (g)'
-                  'par portion de 100 grammes',
-                ),
-                const Divider(),
-                Table(
-                  columnWidths: <int, TableColumnWidth>{
-                    0: FixedColumnWidth((window.physicalSize.width / 100 * 70)),
-                    1: const IntrinsicColumnWidth(),
-                  },
-                  defaultVerticalAlignment: TableCellVerticalAlignment.middle,
-                  children: <TableRow>[
-                    const TableRow(
-                      children: [
-                        TableCell(
-                          child: Text(
-                            'Nutriment',
-                          ),
-                        ),
-                        TableCell(
-                          child: Text(
-                            'Quantité (en g)',
-                          ),
-                        ),
-                      ],
-                    ),
-                    TableRow(
-                      children: [
-                        const TableCell(
-                          child: Text(
-                            'Glucides',
-                          ),
-                        ),
-                        TableCell(
-                          verticalAlignment: TableCellVerticalAlignment.middle,
-                          child: Text(
-                            product.carboHydrates.toString(),
-                          ),
-                        ),
-                      ],
-                    ),
-                    TableRow(
-                      children: [
-                        const TableCell(
-                          child: Text('Protides'),
-                        ),
-                        TableCell(
-                          child: Text(
-                            product.protein.toString(),
-                          ),
-                        ),
-                      ],
-                    ),
-                    TableRow(
-                      children: [
-                        const TableCell(
-                          child: Text(
-                            'Lipides',
-                          ),
-                        ),
-                        TableCell(
-                          child: Text(
-                            product.fat.toString(),
-                          ),
-                        ),
-                      ],
-                    ),
-                    // TableRow(
-                    //   children: [
-                    //     const Text('Vitamine A'),
-                    //     Text(product.vitaminA.toString()),
-                    //   ],
-                    // ),
-                    // TableRow(
-                    //   children: [
-                    //     const Text('Vitamine B1'),
-                    //     Text(product.vitaminB1.toString()),
-                    //   ],
-                    // ),
-                    // TableRow(
-                    //   children: [
-                    //     const Text('Vitamine B2'),
-                    //     Text(product.vitaminB2.toString()),
-                    //   ],
-                    // ),
-                    // TableRow(
-                    //   children: [
-                    //     const Text('Vitamine B6'),
-                    //     Text(product.vitaminB6.toString()),
-                    //   ],
-                    // ),
-                    // TableRow(
-                    //   children: [
-                    //     const Text('Vitamine B9'),
-                    //     Text(product.vitaminB9.toString()),
-                    //   ],
-                    // ),
-                    // TableRow(
-                    //   children: [
-                    //     const Text('Vitamine C'),
-                    //     Text(product.vitaminC.toString()),
-                    //   ],
-                    // ),
-                    // TableRow(
-                    //   children: [
-                    //     const Text('Vitamine D'),
-                    //     Text(product.vitaminD.toString()),
-                    //   ],
-                    // ),
-                    // TableRow(
-                    //   children: [
-                    //     const Text('Vitamine E'),
-                    //     Text(product.vitaminE.toString()),
-                    //   ],
-                    // ),
-                    // TableRow(
-                    //   children: [
-                    //     const Text('Vitamine K'),
-                    //     Text(product.vitaminK.toString()),
-                    //   ],
-                    // ),
-                    // TableRow(
-                    //   children: [
-                    //     const Text('Vitamine PP'),
-                    //     Text(product.vitaminPP.toString()),
-                    //   ],
-                    // ),
-                  ],
-                )
-              ],
-            );
-          } else if (snapshot.hasError) {
-            return const Center(
-              child: Text(
-                'Une erreur est survenu lors du chargement des données',
-              ),
-            );
-          } else {
-            return const CircularProgressIndicator();
-          }
-        },
+          },
+        ),
       ),
     );
   }
